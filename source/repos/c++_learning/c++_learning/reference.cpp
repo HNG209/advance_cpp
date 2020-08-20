0a8c7508acd5b5a6fe848d013e2ab65d2b0c3e4a
unknown <tranphuchung29o92oo4@gmail.com>
2020-08-20 18:04:29 +0700

    reference in cpp

---
+++ source/repos/c++_learning/c++_learning/reference.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+void do_sum(float& sum) {//truyền tham chiếu
+	float a, b;
+	cout << "enter a:";
+	cin >> a;
+	cout << "enter b:";
+	cin >> b;
+	sum = a + b;
+}
+void do_Sum(float* sum) {//truyền con trỏ
+	float a;
+	float b;
+	cout << "enter a:";
+	cin >> a;
+	cout << "enter b:";
+	cin >> b;
+	*sum = a + b;
+}
+int main() {
+	float sum;
+	do_Sum(&sum);
+	cout << "sum is " << sum << endl;
+	return 0;
+}
